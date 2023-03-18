17cfbeb17ef049c8de1e6b57078385fa573dc3f6
Martypose <martinpose@hotmail.com>
2023-03-18 05:34:45 +0100

    Cliente socket, no servidor

--- socketio_handling.py
+++ socketio_handling.py
@@ -21,4 +21,12 @@ def esta_conectado():  # Función para devolver el estado de cliente_conectado
     global cliente_conectado
     return cliente_conectado
 
-sio.connect('https://www.maderaexteriores.com')
+# Función para enviar un evento
+def enviar_evento(nombre_evento, datos):
+    sio.emit(nombre_evento, datos)
+
+# Conectarse al servidor Socket.IO en el lado del servidor de Flask
+sio.connect('https://www.maderaexteriores.com/api')
+
+# Enviar un evento al servidor Socket.IO en el lado del servidor de Flask
+enviar_evento('evento', {'mensaje': 'Hola desde Flask'})
