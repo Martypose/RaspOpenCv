d087154f825fa9f665f110938fcb68e0e5db945e
Martypose <martinpose@hotmail.com>
2023-03-18 18:37:36 +0100

    s

--- socketio_handling.py
+++ socketio_handling.py
@@ -36,7 +36,7 @@ def enviar_evento(nombre_evento, datos):
     sio.emit(nombre_evento, datos)
 
 # Conectarse al servidor Socket.IO en el lado del servidor de Flask
-sio.connect('http://www.maderaexteriores.com/socket.io/')
+sio.connect('https://www.maderaexteriores.com/socket.io/', transports=["websocket"])
 
 # Enviar un evento al servidor Socket.IO en el lado del servidor de Flask
 enviar_evento('evento', {'mensaje': 'Hola desde Flask'})
